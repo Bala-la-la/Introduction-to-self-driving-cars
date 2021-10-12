27d3746a69604bb8038e6348571564c7c762ff44
Bala-la-la <2277133608@qq.com>
2021-10-12 10:41:31 +0800

    无

--- Course1FinalProject/controller2d.py
+++ Course1FinalProject/controller2d.py
@@ -7,47 +7,48 @@
 import cutils
 import numpy as np
 
+
 class Controller2D(object):
     def __init__(self, waypoints):
-        self.vars                = cutils.CUtils()
-        self._current_x          = 0
-        self._current_y          = 0
-        self._current_yaw        = 0
-        self._current_speed      = 0
-        self._desired_speed      = 0
-        self._current_frame      = 0
-        self._current_timestamp  = 0
+        self.vars = cutils.CUtils()
+        self._current_x = 0
+        self._current_y = 0
+        self._current_yaw = 0
+        self._current_speed = 0
+        self._desired_speed = 0
+        self._current_frame = 0
+        self._current_timestamp = 0
         self._start_control_loop = False
-        self._set_throttle       = 0
-        self._set_brake          = 0
-        self._set_steer          = 0
-        self._waypoints          = waypoints
-        self._conv_rad_to_steer  = 180.0 / 70.0 / np.pi
-        self._pi                 = np.pi
-        self._2pi                = 2.0 * np.pi
+        self._set_throttle = 0
+        self._set_brake = 0
+        self._set_steer = 0
+        self._waypoints = waypoints
+        self._conv_rad_to_steer = 180.0 / 70.0 / np.pi
+        self._pi = np.pi
+        self._2pi = 2.0 * np.pi
 
     def update_values(self, x, y, yaw, speed, timestamp, frame):
-        self._current_x         = x
-        self._current_y         = y
-        self._current_yaw       = yaw
-        self._current_speed     = speed
+        self._current_x = x
+        self._current_y = y
+        self._current_yaw = yaw
+        self._current_speed = speed
         self._current_timestamp = timestamp
-        self._current_frame     = frame
+        self._current_frame = frame
         if self._current_frame:
             self._start_control_loop = True
 
     def update_desired_speed(self):
-        min_idx       = 0
-        min_dist      = float("inf")
+        min_idx = 0
+        min_dist = float("inf")
         desired_speed = 0
         for i in range(len(self._waypoints)):
             dist = np.linalg.norm(np.array([
-                    self._waypoints[i][0] - self._current_x,
-                    self._waypoints[i][1] - self._current_y]))
+                self._waypoints[i][0] - self._current_x,
+                self._waypoints[i][1] - self._current_y]))
             if dist < min_dist:
                 min_dist = dist
                 min_idx = i
-        if min_idx < len(self._waypoints)-1:
+        if min_idx < len(self._waypoints) - 1:
             desired_speed = self._waypoints[min_idx][2]
         else:
             desired_speed = self._waypoints[-1][2]
@@ -61,7 +62,7 @@ class Controller2D(object):
 
     def set_throttle(self, input_throttle):
         # Clamp the throttle command to valid bounds
-        throttle           = np.fmax(np.fmin(input_throttle, 1.0), 0.0)
+        throttle = np.fmax(np.fmin(input_throttle, 1.0), 0.0)
         self._set_throttle = throttle
 
     def set_steer(self, input_steer_in_rad):
@@ -69,29 +70,29 @@ class Controller2D(object):
         input_steer = self._conv_rad_to_steer * input_steer_in_rad
 
         # Clamp the steering command to valid bounds
-        steer           = np.fmax(np.fmin(input_steer, 1.0), -1.0)
+        steer = np.fmax(np.fmin(input_steer, 1.0), -1.0)
         self._set_steer = steer
 
     def set_brake(self, input_brake):
         # Clamp the steering command to valid bounds
-        brake           = np.fmax(np.fmin(input_brake, 1.0), 0.0)
+        brake = np.fmax(np.fmin(input_brake, 1.0), 0.0)
         self._set_brake = brake
 
     def update_controls(self):
         ######################################################
         # RETRIEVE SIMULATOR FEEDBACK
         ######################################################
-        x               = self._current_x
-        y               = self._current_y
-        yaw             = self._current_yaw
-        v               = self._current_speed
+        x = self._current_x
+        y = self._current_y
+        yaw = self._current_yaw
+        v = self._current_speed
         self.update_desired_speed()
-        v_desired       = self._desired_speed
-        t               = self._current_timestamp
-        waypoints       = self._waypoints
+        v_desired = self._desired_speed
+        t = self._current_timestamp
+        waypoints = self._waypoints
         throttle_output = 0
-        steer_output    = 0
-        brake_output    = 0
+        steer_output = 0
+        brake_output = 0
 
         ######################################################
         ######################################################
@@ -145,7 +146,7 @@ class Controller2D(object):
 
                                           waypoints[5]:
                                           Returns [x5, y5, v5] (6th waypoint)
-                
+
                 Controller Output Variables:
                     throttle_output : Throttle output (0 to 1)
                     steer_output    : Steer output (-1.22 rad to 1.22 rad)
@@ -162,13 +163,13 @@ class Controller2D(object):
                 access the persistent variables declared above here. For
                 example, can treat self.vars.v_previous like a "global variable".
             """
-            
+
             # Change these outputs with the longitudinal controller. Note that
             # brake_output is optional and is not required to pass the
             # assignment, as the car will naturally slow down over time.
             K_p = 0.2
             K_d = 0.05
-            k_i = 0.01
+            K_i = 0.01
 
             error_v = self._desired_speed - self._current_speed
             self.vars.e_total += error_v
@@ -190,56 +191,77 @@ class Controller2D(object):
                 access the persistent variables declared above here. For
                 example, can treat self.vars.v_previous like a "global variable".
             """
-            
+
             # Change the steer output with the lateral controller.
 
-            # Choose last waypoint as initial target
-            k = 0.3
             idx = len(self._waypoints) - 1
-            #最近点的距离
             dist = 10000000
             laterr = 0
             idy = 0
-            robot_state = np.zeros(2)
-            robot_state[0] = self._current_x + 2 * np.cos(self._current_yaw)
-            robot_state[1] = self._current_y + 2 * np.sin(self._current_yaw)
 
             # Find the nearest waypoint
             for i in range(len(self._waypoints)):
                 dist1 = np.sqrt(
-                    (self._waypoints[i][0] - robot_state[0]) ** 2 + (self._waypoints[i][1] - robot_state[1]) ** 2)
+                    (self._waypoints[i][0] - self._current_x) ** 2 + (self._waypoints[i][1] - self._current_y) ** 2)
                 if dist1 < dist:
                     dist = dist1
                     idx = i
 
-            if self._waypoints[idx][1] > robot_state[1]:
-                laterr = self._waypoints[idx][1] - robot_state[1]
-            else:
-                laterr = -(self._waypoints[idx][1] - robot_state[1])
-
-            if idx < len(self._waypoints) - 6:
-                idy = idx+5
+            if idx < len(self._waypoints) - 18:
+                idy = idx + 17
             else:
                 idy = len(self._waypoints) - 1
+            #取得是点到平行线的距离作为横向误差
+            alpha = np.arctan2(waypoints[idy][1] - waypoints[idx][1], waypoints[idy][0] - waypoints[idx][0])
+            laterr = (self._current_x - waypoints[idx][0]) * np.sin(alpha) + (
+                        self._current_y - waypoints[idx][1]) * np.cos(alpha)
 
-            theta_fai = self.steer_output_p - np.arctan((self._waypoints[idy][1] - robot_state[1]) / (self._waypoints[idy][0] - robot_state[0]))
+            yaw_path = np.arctan2(waypoints[idy][1] - waypoints[idx][1], waypoints[idy][0] - waypoints[idx][0])
+            theta_fai = yaw_path - yaw
+
             if theta_fai > np.pi:
-                theta_fai = theta_fai - 2*np.pi
+                theta_fai = theta_fai - 2 * np.pi
             elif theta_fai < -np.pi:
                 theta_fai = theta_fai + 2 * np.pi
             else:
                 theta_fai = theta_fai
-            theta_y = np.arctan(5*laterr/self._current_speed)
+
+            crosstrack_error = laterr
+            yaw_cross_track = np.arctan2(y - waypoints[0][1], x - waypoints[0][0])
+            yaw_path2ct = np.arctan2(waypoints[-1][1] - waypoints[0][1],
+                                     waypoints[-1][0] - waypoints[0][0]) - yaw_cross_track
+            if yaw_path2ct > np.pi:
+                yaw_path2ct -= 2 * np.pi
+            if yaw_path2ct < - np.pi:
+                yaw_path2ct += 2 * np.pi
+            if theta_fai > 0:
+                crosstrack_error = abs(crosstrack_error)
+            else:
+                crosstrack_error = - abs(crosstrack_error)
+            yaw_diff_crosstrack = np.arctan(5 * crosstrack_error / (v))
+
+            # final expected steering
+            steer_expect = yaw_diff_crosstrack + theta_fai
+            if steer_expect > np.pi:
+                steer_expect -= 2 * np.pi
+            if steer_expect < - np.pi:
+                steer_expect += 2 * np.pi
+            steer_expect = min(1.22, steer_expect)
+            steer_expect = max(-1.22, steer_expect)
+
+            # update
+            steer_output = steer_expect
 
             # Set steering angle
-            steer_output = theta_fai + theta_y
+            # steer_output = theta_fai + theta_y
+            s1 = steer_output
 
             ######################################################
             # SET CONTROLS OUTPUT
             ######################################################
             self.set_throttle(throttle_output)  # in percent (0 to 1)
-            self.set_steer(steer_output)        # in rad (-1.22 to 1.22)
-            self.set_brake(brake_output)        # in percent (0 to 1)
+            self.set_steer(steer_output)  # in rad (-1.22 to 1.22)
+            self.set_brake(brake_output)  # in percent (0 to 1)
 
         ######################################################
         ######################################################
@@ -252,4 +274,4 @@ class Controller2D(object):
             in the next iteration)
         """
         self.vars.v_previous = v
-        self.vars.steer_output_p = steer_output # Store forward speed to be used in next step
+        self.vars.steer_output_p = s1  # Store forward speed to be used in next step
